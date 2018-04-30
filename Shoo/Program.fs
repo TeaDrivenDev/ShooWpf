0b98a136226b7880579b84fccd30c4534ddca80d
TeaDrivenDev <TeaDrivenDev@gmail.com>
2018-04-30 05:06:15 +0200

    Allow selecting folders via dialog

--- Shoo/Program.fs
+++ Shoo/Program.fs
@@ -1,14 +1,45 @@
 ﻿namespace Shoo
 
+open System
 open System.Windows
 
 open FsXaml
+
+open Shoo.ViewModels
+
+type Win32Window(handle : IntPtr) =
+    interface System.Windows.Forms.IWin32Window with
+        member __.Handle : IntPtr = handle
 
 type MainWindowBase = XAML<"MainWindow.xaml">
 
 type MainWindow() as this =
     inherit MainWindowBase()
 
+    let selectFolder previous =
+        use folderBrowserDialog = new Forms.FolderBrowserDialog()
+        folderBrowserDialog.ShowNewFolderButton <- false
+
+        if not <| String.IsNullOrWhiteSpace previous && System.IO.Directory.Exists previous
+        then
+            folderBrowserDialog.SelectedPath <- previous
+
+        let win = Win32Window(System.Windows.Interop.WindowInteropHelper(this).Handle)
+        let result = folderBrowserDialog.ShowDialog win
+
+        if result = Forms.DialogResult.OK
+        then Some folderBrowserDialog.SelectedPath
+        else None
+
+    member __.ViewModel : MainWindowViewModel = __.DataContext :?> MainWindowViewModel
+
+    override __.SelectSourceDirectory_Click (_ : obj, e : RoutedEventArgs) =
+        selectFolder __.ViewModel.SourceDirectory.Value
+        |> Option.iter (fun directory -> __.ViewModel.SourceDirectory.Value <- directory)
+
+    override __.SelectDestinationDirectory_Click (_ : obj, e : RoutedEventArgs) =
+        selectFolder __.ViewModel.DestinationDirectory.Value
+        |> Option.iter (fun directory -> __.ViewModel.DestinationDirectory.Value <- directory)
 
 type AppBase = XAML<"App.xaml">
 
