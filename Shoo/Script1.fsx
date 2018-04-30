0a72fa1d5e76942534e0aa77f9ee8e532f39ca23
TeaDrivenDev <TeaDrivenDev@gmail.com>
2018-04-30 03:03:06 +0200

    Initial commit

---
+++ Shoo/Script1.fsx
@@ -0,0 +1,24 @@
+﻿
+open System
+open System.IO
+open System.Net
+
+
+let copy source destinationDirectory =
+    let client = new WebClient()
+    client.DownloadProgressChanged.Add (fun (e : DownloadProgressChangedEventArgs) ->
+        if e.ProgressPercentage % 10 = 0
+        then
+            printfn "%i %%" e.ProgressPercentage)
+    client.DownloadDataCompleted.Add (fun (e : DownloadDataCompletedEventArgs) ->
+        printfn "\nDone")
+
+    let destination = Path.Combine(destinationDirectory, Path.GetFileName source)
+
+    client.DownloadFileAsync(Uri source, destination)
+
+
+let source = @"E:\Trine_Setup_109.zip"
+let destinationDirectory = @"D:\"
+
+copy source destinationDirectory
---
+++ build.fsx
@@ -0,0 +1,38 @@
+System.IO.Directory.SetCurrentDirectory __SOURCE_DIRECTORY__
+
+#r @"packages/build/FAKE/tools/FakeLib.dll"
+
+open System
+open System.IO
+
+open Fake
+open Fake.Git
+open Fake.Testing
+
+let solutionFile  = "Shoo.sln"
+
+let gitOwner = "TeaDrivenDev"
+let gitHome = "https://github.com/" + gitOwner
+let gitName = "Shoo"
+let gitRaw = environVarOrDefault "gitRaw" ("https://raw.github.com/" + gitOwner)
+
+let outputDirectory = "bin"
+
+Target "Clean" (fun _ -> CleanDirs [ outputDirectory ])
+
+Target "Build" (fun _ ->
+    !! solutionFile
+    |> MSBuildRelease "" "Rebuild"
+    |> ignore)
+
+//Target "RunTests" (fun _ ->
+//    !! "**/bin/Release/**/*.Tests.dll"
+//    |> xUnit2 id)
+
+//Target "All" DoNothing
+
+"Build"
+//==> "RunTests"
+//==> "All"
+
+RunTargetOrDefault "Build"
