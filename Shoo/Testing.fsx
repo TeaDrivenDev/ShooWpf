685c281f92abfc2dd42476a9281775b0375ae2a2
TeaDrivenDev <TeaDrivenDev@gmail.com>
2018-05-01 04:34:19 +0200

    Add testing script

---
+++ Shoo/Testing.fsx
@@ -0,0 +1,25 @@
+﻿open System
+open System.IO
+
+let copySource = @"E:\Staging\Shoo\Source"
+let watching = @"E:\Staging\Shoo\Watching"
+
+//copySource
+//|> Directory.GetFiles
+//|> Seq.iter (fun name -> File.Move(name, name + "_"))
+
+watching
+|> Directory.GetFiles
+|> Seq.iter File.Delete
+
+copySource
+|> Directory.GetFiles
+|> Seq.truncate 4
+|> Seq.map (fun filePath ->
+    let fileName = Path.GetFileName filePath
+    let newFilePath = Path.Combine(watching, fileName)
+    
+    filePath, newFilePath)
+|> Seq.iter (fun (old, newP) ->
+    File.Copy(old, newP)
+    File.Move(newP, newP.Substring(0, newP.Length - 1)))
